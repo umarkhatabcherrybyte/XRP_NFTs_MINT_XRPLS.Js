23b640aab4f8a6e53f4b02f6c896d7c06aad2a1c
Umar Khatab <umarkhatab@thecherrybyte.com>
2023-09-08 15:20:47 +0500

    added burn and get transaction information

--- pages/components/AccountManager.js
+++ pages/components/AccountManager.js
@@ -9,17 +9,18 @@ import { VStack } from "@chakra-ui/react";
 import { Xumm } from "xumm";
 const xrpl = require("xrpl")
 
-var xumm = new Xumm('2cc1e927-2d8e-4c9e-aa40-6ba3b75143e5', 'd44c0da1-63de-4acc-b223-ab1b58699588')
+var xumm = new Xumm('f4da7833-89a4-494d-a121-61d6eb020888', '182c3a31-f2d3-406d-98d9-0f3b95c62aba')
 
 function AccountManager() {
   const [selectedChain, setSelectedChain] = useState(chains[0].name);
   const [xrplClient, setXRPLClient] = useState(null);
   const [connectedAddress, setConnectedAddress] = useState(null)
-/** 
- * 
- * Backend
-*/
-  async function mintTheNFT(xrplClient_,XummClient) {
+  /** 
+   * 
+   * Backend
+  */
+  async function mintTheNFT(xrplClient_, XummClient) {
+    console.log("xumm client", XummClient);
     let accountInfo = await xrplClient_.request({
       command: "account_info",
       account: connectedAddress,
@@ -47,11 +48,48 @@ function AccountManager() {
       }
     })
 
+    // const { created, resolved } = await XummClient.payload.createAndSubscribe({
+    //   "Account": connectedAddress,
+    //   TransactionType: 'NFTokenBurn',
+    //   NFTokenID: "00080000897191DDF525A63AA3C8E114291F89907F43EF1808AB7FC000000065",
+    //   Owner: connectedAddress
+    // }, eventMessage => {
+    //   if (Object.keys(eventMessage.data).indexOf('opened') > -1) {
+    //     // Update the UI? The payload was opened.
+    //   }
+    //   if (Object.keys(eventMessage.data).indexOf('signed') > -1) {
+    //     // The `signed` property is present, true (signed) / false (rejected)
+    //     return eventMessage
+    //   }
+    // })
+
+
     console.log('Payload URL:', created.next.always)
     console.log('Payload QR:', created.refs.qr_png)
     const payload = await resolved;
     console.log('Resolved', payload)
+    console.log("Getting transaction details");
+    const ledger = await xrplClient_.request({
+      command: 'ledger',
+      transactions: true,
+      ledger_index: 'validated',
+    })
+    console.log("Latest validated ledger:", ledger)
+
+    const transactions = ledger.result.ledger.transactions
+    console.log("all transactions", transactions);
+
+      if (transactions) {
+        const tx = await xrplClient_.request({
+          command: 'tx',
+          transaction: transactions[0],
+        })
+
+        console.log("First transaction in the ledger:")
+        console.log(tx)
+      }
   }
+
 
   /**--------------------- */
   /******* FrontEnd ********/
@@ -61,10 +99,10 @@ function AccountManager() {
     return client;
 
   }
-  async function mint(){
-    await mintTheNFT(xrplClient,xumm)
+  async function mint() {
+    await mintTheNFT(xrplClient, xumm)
   }
-  
+
   async function init() {
     let _client = await connectXRPL(setXRPLClient, selectedChain);
     setXRPLClient(_client)
@@ -114,7 +152,7 @@ function AccountManager() {
 
       }
 
-      
+
     </VStack>
   );
 }
